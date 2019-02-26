e77941dcc59180a230ba88b376ed5480e6bbe7e0
Toderutiu <radu.toderutiu@bearingpoint.com>
2019-02-26 23:49:34 +0100

    Error Handling

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -47,6 +47,7 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Bla");
 
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
@@ -77,9 +78,7 @@ namespace DatingApp.API.Controllers
             return  Ok(new {
                 token = tokenhandler.WriteToken(token)
             });
-
-
-
+            
         }
 
     }
