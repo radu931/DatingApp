f0fdc69e8c6f2242b69e470342ab3da27bfcf06b
Toderutiu <radu.toderutiu@bearingpoint.com>
2019-04-21 13:48:00 +0200

    End Of Section 6

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -55,7 +55,7 @@ namespace DatingApp.API.Controllers
             var claims = new[]
             {
                 new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
-                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Username)
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8
